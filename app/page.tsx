7d70c64e689ff16fa12929132012bb8d521907ab
Lingikaushikreddy <Kaushikreddylingireddy2002@gmail.com>
2025-12-27 20:41:24 -0500

    Add Home Page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { SplineSceneBasic } from "@/components/ui/demo";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black p-4">
+      <SplineSceneBasic />
+    </main>
+  );
+}
